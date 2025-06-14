cc1a1570ff4410eb1aed6a2e03a1ee2cb4f241c9
aniketh <aniketh8@illinois.edu>
2025-06-13 21:18:04 -0700

    finished smore

---
+++ verilog-language/vectors/0017.v
@@ -0,0 +1,12 @@
+module top_module( 
+    input [7:0] in,
+    output [7:0] out
+);
+genvar i;
+generate 
+    for (i = 0; i <= 7; i = i + 1) begin
+        assign out[i] = in[7 - i];
+    end
+endgenerate
+
+endmodule
